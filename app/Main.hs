cc5fed925d815c5184c79c4b85d66d906177ea80
Eyal Lotem <eyal.lotem@gmail.com>
2016-03-02 11:33:51 +0200

    Main: Remove superfluous Alex parsing phase. Style

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,9 @@
 module Main (main) where
 
 import           BMake.Base (Unit, UnitF(..), Statement, StatementF(..), substmts)
-import           BMake.User (Error, parseUnit, parseWithAlex, stateBase)
+import           BMake.User (Error, parseUnit)
 import           Control.DeepSeq (force)
 import           Control.Exception (evaluate)
-import           Control.Monad (forM_)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as BL8
@@ -68,22 +67,17 @@ memoIO cache action k =
                     return v
 
 parse :: ParseCache -> FilePath -> FilePath -> IO Unit
-parse cache rootDir = memoIO cache $ \makefile -> do
-    content <- BL.readFile makefile
-    let initialParse = parseWithAlex stateBase content
-    putStrLn makefile
-    let dirs = Dirs rootDir (FilePath.takeDirectory makefile)
-    case initialParse of
-        Left str -> fail str
-        Right tokens -> do
-            res <- parseSingle content
-            case res of
-                Left x -> do
-                    forM_ tokens print
-                    fail $ show x
-                Right ast -> do
-                    ast' <- Unit <$> handleIncludes cache dirs (unit ast)
-                    return ast'
+parse cache rootDir =
+    memoIO cache $ \makefile -> do
+        content <- BL.readFile makefile
+        putStrLn makefile
+        let dirs = Dirs rootDir (FilePath.takeDirectory makefile)
+        res <- parseSingle content
+        case res of
+            Left x -> fail $ show x
+            Right ast -> do
+                ast' <- Unit <$> handleIncludes cache dirs (unit ast)
+                return ast'
 
 main :: IO ()
 main = do
